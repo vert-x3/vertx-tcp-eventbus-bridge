373ea367e7f77a22291df5eb50db6e54669d4246
Toby Crawley <toby@tcrawley.org>
2016-12-19 13:41:09 -0500

    Only deliver sends to one handler in the node client
    This uses the new `send` key to know if a message should be delivered to
    one or all handlers. This also fixes the behavior when an error message
    is received - error messages that are directed at addresses are of type
    "message", not "err". "err" messages a general, and have no address, so
    should only go to the global error handler.
    
    Related to #20
    
    Signed-off-by: Toby Crawley <toby@tcrawley.org>

--- src/client/nodejs/test/TcpEventBusBridgeEchoServer.java
+++ src/client/nodejs/test/TcpEventBusBridgeEchoServer.java
@@ -1,6 +1,7 @@
 package test;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.bridge.BridgeOptions;
@@ -11,20 +12,32 @@ public class TcpEventBusBridgeEchoServer {
 
   public static void main(String[] args) {
     Vertx vertx = Vertx.vertx();
+    EventBus eb = vertx.eventBus();
 
-    vertx.eventBus().consumer("hello", (Message<JsonObject> msg) -> {
+    eb.consumer("hello", (Message<JsonObject> msg) -> {
       msg.reply(new JsonObject().put("value", "Hello " + msg.body().getString("value")));
     });
 
-    vertx.eventBus().consumer("echo",
+    eb.consumer("echo",
         (Message<JsonObject> msg) -> msg.reply(msg.body()));
+
+    eb.consumer("echo2",
+                (Message<JsonObject> msg) -> {
+                  if ("send".equals(msg.body().getString("response_type"))) {
+                    eb.send("echo2_response", msg.body());
+                  } else {
+                    eb.publish("echo2_response", msg.body());
+                  }
+                });
 
     TcpEventBusBridge bridge = TcpEventBusBridge.create(
         vertx,
         new BridgeOptions()
             .addInboundPermitted(new PermittedOptions().setAddress("hello"))
             .addInboundPermitted(new PermittedOptions().setAddress("echo"))
-            .addOutboundPermitted(new PermittedOptions().setAddress("echo")));
+            .addOutboundPermitted(new PermittedOptions().setAddress("echo"))
+            .addInboundPermitted(new PermittedOptions().setAddress("echo2"))
+            .addOutboundPermitted(new PermittedOptions().setAddress("echo2_response")));
 
     bridge.listen(7000, res -> System.out.println("Ready"));
   }
