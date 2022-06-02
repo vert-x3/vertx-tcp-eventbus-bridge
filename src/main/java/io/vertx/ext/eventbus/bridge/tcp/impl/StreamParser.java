fe372fbf4ac7399f427eb8bdff9d9d2c5ddf0ad8
Paulo Lopes <pmlopes@gmail.com>
2022-06-02 13:45:43 +0200

    Update parser to not rely on line endings but message boundaries
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

--- src/main/java/io/vertx/ext/eventbus/bridge/tcp/impl/StreamParser.java
+++ src/main/java/io/vertx/ext/eventbus/bridge/tcp/impl/StreamParser.java
@@ -20,25 +20,19 @@ import io.vertx.core.buffer.Buffer;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
 
-import java.nio.charset.StandardCharsets;
-
 public final class StreamParser implements Handler<Buffer> {
 
-  private static final Logger log = LoggerFactory.getLogger(StreamParser.class);
-
-  private static final Buffer EMPTY = Buffer.buffer(0);
-  private static final byte[] CONTENT_TYPE = "Content-Type:".toLowerCase().getBytes();
-  private static final byte[] CONTENT_LENGTH = "Content-Length:".toLowerCase().getBytes();
+  private static final Logger LOGGER = LoggerFactory.getLogger(StreamParser.class);
 
   // the callback when a full response message has been decoded
-  private ParserHandler handler;
+  private Handler<Buffer> handler;
   private Handler<Throwable> exceptionHandler;
 
   // a composite buffer to allow buffer concatenation as if it was
   // a long stream
   private final ReadableBuffer buffer = new ReadableBuffer();
 
-  public StreamParser handler(ParserHandler handler) {
+  public StreamParser handler(Handler<Buffer> handler) {
     this.handler = handler;
     return this;
   }
@@ -48,123 +42,51 @@ public final class StreamParser implements Handler<Buffer> {
     return this;
   }
 
-  // parser state machine state
-  private boolean eol = true;
-  private int contentLength = 0;
-  private String contentType = null;
-
   @Override
   public void handle(Buffer chunk) {
-    // add the chunk to the buffer
-    buffer.append(chunk);
+    if (chunk.length() > 0) {
+      // add the chunk to the buffer
+      buffer.append(chunk);
 
-    while (buffer.readableBytes() >= (eol ? 3 : contentLength)) {
-      // setup a rollback point
-      buffer.mark();
+      // the minimum messages are "{}" or "[]"
+      while (buffer.readableBytes() >= 2) {
+        // setup a rollback point
+        buffer.mark();
 
-      // we need to locate the eol
-      if (eol) {
-        // locate the eol and handle as a C string
-        final int start = buffer.offset();
-        final int eol = buffer.findLineEnd(); // including \r\n
-        final int end = eol - 2;
+        final Buffer payload;
 
-        // not found at all
-        if (eol == -1) {
-          buffer.reset();
+        try {
+          // locate the message boundaries
+          final int start = buffer.findSTX();
+
+          // no start found yet
+          if (start == -1) {
+            buffer.reset();
+            break;
+          }
+
+          final int end = buffer.findETX(start);
+
+          // no end found yet
+          if (end == -1) {
+            buffer.reset();
+            break;
+          }
+
+          payload = buffer.readBytes(start, end - start);
+        } catch (IllegalStateException ise) {
+          exceptionHandler.handle(ise);
           break;
         }
 
-        final int length = eol - start - 2;
-
-        if (length == 0) {
-          // switch from headers to content
-          this.eol = false;
-        } else {
-          // process line
-          if (
-            ('{' == buffer.getByte(start) && '}' == buffer.getByte(end - 1)) ||
-            ('[' == buffer.getByte(start) && ']' == buffer.getByte(end - 1))
-          ) {
-            // special case, when no headers were used
-            Buffer payload = buffer.readBytes(length);
-            if (handler != null) {
-              try {
-                handler.handle(contentType, payload);
-              } catch (RuntimeException e) {
-                log.error("Cannot handle message", e);
-              }
-            } else {
-              log.info("No handler expecting: " + payload);
-            }
-            // reset as this was a message without headers
-            contentType = null;
-            contentLength = 0;
-          }
-          else if (buffer.startsWith(CONTENT_TYPE, end)) {
-            // guaranteed to be possible as the startswith passed
-            buffer.skip(13);
-            // skip white space
-            while (buffer.getByte(buffer.offset()) == ' ') {
-              buffer.skip(1);
-            }
-            contentType = buffer.readLine(end, StandardCharsets.UTF_8);
-          }
-          else if (buffer.startsWith(CONTENT_LENGTH, end)) {
-            // guaranteed to be possible as the startswith passed
-            buffer.skip(15);
-            // skip white space
-            while (buffer.getByte(buffer.offset()) == ' ') {
-              buffer.skip(1);
-            }
-
-            try {
-              // set the expected content length
-              contentLength = buffer.readInt(end);
-            } catch (RuntimeException e) {
-              if (exceptionHandler != null) {
-                exceptionHandler.handle(e);
-              } else {
-                log.error("Cannot parse Content-Length", e);
-              }
-              return;
-            }
-          } else {
-            log.warn("Unhandled header: " + buffer.readLine(end, StandardCharsets.UTF_8));
-          }
+        // payload is found, deliver it to the handler
+        try {
+          handler.handle(payload);
+        } catch (RuntimeException e) {
+          // these are user exceptions, not protocol exceptions
+          // we can continue the parsing job
+          LOGGER.error("Failed to handle payload", e);
         }
-        // skip \r\n
-        buffer.skip(2);
-      } else {
-        // empty string
-        if (contentLength == 0) {
-          // special case as we don't need to allocate objects for this
-          if (handler != null) {
-            try {
-              handler.handle(contentType, EMPTY);
-            } catch (RuntimeException e) {
-              log.error("Cannot handle message", e);
-            }
-          } else {
-            log.info("No handler expecting: \"\"");
-          }
-        } else {
-          // fixed length parsing && read the required bytes
-          Buffer b = buffer.readBytes(contentLength);
-          if (handler != null) {
-            try {
-              handler.handle(contentType, b);
-            } catch (RuntimeException e) {
-              log.error("Cannot handle message", e);
-            }
-          } else {
-            log.info("No handler expecting: " + b);
-          }
-        }
-        // switch back to eol parsing
-        eol = true;
-        contentType = null;
-        contentLength = 0;
       }
     }
   }
