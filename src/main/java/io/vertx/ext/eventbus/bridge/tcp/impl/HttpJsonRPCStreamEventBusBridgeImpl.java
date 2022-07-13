53b887f01e6a5defe110d505d6e666742a5675e2
Lucifer Morningstar <lucifermorningstar4j@gmail.com>
2022-07-13 17:08:29 +0530

    Remove endHandler from HttpRequest handler
    For http request, end handler will always be invoked after the request
    data has been read. While for websockets and tcp sockets, it is only
    invoked at end of connection. So, it makes sense to have it in the latter
    but not in http transport. Also, the actions we took in the end handler
    was remove existing subscriptions but that should actually be done
    when response has ended. Therefore, added it as bodyHandler to response
    instead.

--- src/main/java/io/vertx/ext/eventbus/bridge/tcp/impl/HttpJsonRPCStreamEventBusBridgeImpl.java
+++ src/main/java/io/vertx/ext/eventbus/bridge/tcp/impl/HttpJsonRPCStreamEventBusBridgeImpl.java
@@ -2,21 +2,20 @@ package io.vertx.ext.eventbus.bridge.tcp.impl;
 
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.eventbus.Message;
 import io.vertx.core.eventbus.MessageConsumer;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
-import io.vertx.core.net.NetSocket;
 import io.vertx.ext.bridge.BridgeEventType;
-import io.vertx.ext.bridge.BridgeOptions;
 import io.vertx.ext.eventbus.bridge.tcp.BridgeEvent;
 import io.vertx.ext.eventbus.bridge.tcp.JsonRPCBridgeOptions;
-import io.vertx.json.schema.JsonSchema;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
 
 public class HttpJsonRPCStreamEventBusBridgeImpl extends JsonRPCStreamEventBusBridgeImpl<HttpServerRequest> {
 
@@ -31,17 +30,15 @@ public class HttpJsonRPCStreamEventBusBridgeImpl extends JsonRPCStreamEventBusBr
       () -> new BridgeEventImpl<>(BridgeEventType.SOCKET_CREATED, null, socket),
       // on success
       () -> {
+        // TODO: make these maps persistent across requests otherwise replies won't work because
+        //  http client cannot reply again in the same request after receiving a response and has
+        //  to make a new request.
         final Map<String, MessageConsumer<?>> registry = new ConcurrentHashMap<>();
         final Map<String, Message<JsonObject>> replies = new ConcurrentHashMap<>();
 
         socket.exceptionHandler(t -> {
           log.error(t.getMessage(), t);
           registry.values().forEach(MessageConsumer::unregister);
-          registry.clear();
-        }).endHandler(v -> {
-          registry.values().forEach(MessageConsumer::unregister);
-          // normal close, trigger the event
-          checkCallHook(() -> new BridgeEventImpl<>(BridgeEventType.SOCKET_CLOSED, null, socket));
           registry.clear();
         }).handler(buffer -> {
           // TODO: handle content type
@@ -58,7 +55,13 @@ public class HttpJsonRPCStreamEventBusBridgeImpl extends JsonRPCStreamEventBusBr
           final Object id = msg.getValue("id");
           HttpServerResponse response = socket
             .response()
-            .putHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+            .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+            .bodyEndHandler(handler -> {
+              registry.values().forEach(MessageConsumer::unregister);
+              // normal close, trigger the event
+              checkCallHook(() -> new BridgeEventImpl<>(BridgeEventType.SOCKET_CLOSED, null, socket));
+              registry.clear();
+            });
           Consumer<Buffer> writer;
           if (method.equals("register")) {
             response.setChunked(true);
